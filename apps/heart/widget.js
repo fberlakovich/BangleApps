68f72355cede09070b295e109cbca4a1c1d4b2cf
Felix Berlakovich <felix@berlakovich.at>
2020-07-23 17:33:17 +0200

    display current heart rate instead of heart icon

--- apps/heart/widget.js
+++ apps/heart/widget.js
@@ -1,6 +1,7 @@
 (() => {
   var settings = {};
   var hrmToggle = true; // toggles once for each reading
+  var current = 0;
   var recFile; // file for heart rate recording
 
   // draw your widget
@@ -10,15 +11,17 @@
     g.setFontAlign(0,0);
     g.clearRect(this.x,this.y,this.x+23,this.y+23);
     g.setColor(hrmToggle?"#ff0000":"#ff8000");
-    g.fillCircle(this.x+6,this.y+6,4); // draw heart left circle
-    g.fillCircle(this.x+16,this.y+6,4); // draw heart right circle
-    g.fillPoly([this.x+2,this.y+8,this.x+20,this.y+8,this.x+11,this.y+18]); // draw heart bottom triangle
+//    g.fillCircle(this.x+6,this.y+6,4); // draw heart left circle
+//    g.fillCircle(this.x+16,this.y+6,4); // draw heart right circle
+//    g.fillPoly([this.x+2,this.y+8,this.x+20,this.y+8,this.x+11,this.y+18]); // draw heart bottom triangle
+    g.drawString(current, 23, 23);
     g.setColor(-1); // change color back to be nice to other apps
   }
 
   function onHRM(hrm) {
     hrmToggle = !hrmToggle;
     WIDGETS["heart"].draw();
+    current = hrm.bpm
     if (recFile) recFile.write([getTime().toFixed(0),hrm.bpm,hrm.confidence].join(",")+"\n");
   }
 
